f275e53a7ca673ba09a11cd9c85c072847ab1e13
2268849815 <2268849815@qq.com>
2020-02-25 13:42:21 +0800

    mpvue-koa

--- mpvue-shop-node/controllers/home/index.js
+++ mpvue-shop-node/controllers/home/index.js
@@ -20,12 +20,40 @@ module.exports = async (ctx) =>{
   const hotGoods = await mysql('nideshop_goods').column('id', 'name', 'list_pic_url', 'retail_price', 'goods_brief').where({
     is_hot: 1
   }).limit(5).select()
+  // 专题精选
+  const topicList = await mysql('nideshop_topic').limit(3).select()
+
+  // 类别列表 ** 好物
+  const categoryList = await mysql('nideshop_category').where({
+    parent_id: 0
+  }).select()
+   const newCategoryList = []
+
+   for (let i = 0; i < categoryList.length; i++ ) {
+     let item = categoryList[i]
+     let childCategoryIds = await mysql('nideshop_category').where({
+       parent_id : item.id
+     }).column('id').select()
+     // 变成数组的形式
+     childCategoryIds = childCategoryIds.map((item) => {
+       return item.id
+     })
+     // 在上屏中找到在childCategroyIds里的七条数据
+     const categoryGoods = await mysql('nideshop_goods').column('id', 'name', 'list_pic_url', 'retail_price').whereIn('category_id', childCategoryIds).limit(7).select()
+     newCategoryList.push({
+       'id': item.id,
+       'name': item.name,
+       'goodsList': categoryGoods
+     })
+   }
   ctx.body ={
     'banner':banner,
     'channel':channel,
     'brandlist':brandlist,
     'newGoods':newGoods,
     'hotGoods' : hotGoods,
+    'topicList' : topicList,
+    'newCategoryList' : newCategoryList
   }
 }
 
--- mpvue-shop/src/pages/index/index.vue
+++ mpvue-shop/src/pages/index/index.vue
@@ -81,6 +81,46 @@
             </ul>
         </div>
     </div>
+    <div class="topicList">
+        <div class="topicList-top">
+            专题精选
+            <span class="icon"></span>
+        </div>
+        <div class="list">
+            <ul>
+                <scroll-view class="scroll-view" :scroll-x="true">
+                    <li v-for="(item, index) in topicList" :key="index" @click="topicdetail(item.id)">
+                        <img :src="item.item_pic_url" alt="">
+                        <div class="btom">
+                            <div>
+                                <p>{{item.title}}</p>
+                                <p>{{item.subtitle}}</p>
+                            </div>
+                            <div>{{item.price_info}}元起</div>
+                        </div>
+                    </li>
+                </scroll-view>
+            </ul>
+        </div>
+    </div>
+    <div class="newcategory">
+        <div class="list" v-for="(item,index) in newCategoryList" :key="index">
+            <div class="head">{{item.name}}好物</div>
+            <div class="sublist">
+                <div v-for="(subitem, subindex) in item.goodsList" :key="subindex">
+                    <img :src="subitem.list_pic_url" alt="">
+                    <p>{{subitem.name}}</p>
+                    <p>{{subitem.retail_price}}元起</p>
+                </div>
+                <div>
+                    <div class="last" >
+                        <p>{{item.name}}好物</p>
+                        <span class="icon"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -95,7 +135,9 @@ export default {
             channel: [],
             brandlist: [],
             newGoods: [],
-            hotGoods: []
+            hotGoods: [],
+            topicList: [],
+            newCategoryList: []
         }
     },
     computed: {
@@ -159,6 +201,8 @@ export default {
           this.brandlist = data.brandlist
           this.newGoods = data.newGoods
           this.hotGoods = data.hotGoods
+          this.topicList = data.topicList
+          this.newCategoryList = data.newCategoryList
         },
         categroyList (id) {
             wx.navigateTo({
@@ -186,7 +230,11 @@ export default {
             })
             }
         },
-
+        topicdetail (id) {
+            wx.navigateTo({
+                url: '/pages/topicdetail/main?id=' + id
+            })
+        }
     }
 }
 </script>
