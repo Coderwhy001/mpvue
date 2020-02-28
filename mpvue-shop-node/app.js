424177c995f2050e5dd9fd1022a3da50f9c7b7dd
2268849815 <2268849815@qq.com>
2020-02-28 13:48:07 +0800

    mpvue

--- mpvue-shop-node/app.js
+++ mpvue-shop-node/app.js
@@ -1,9 +1,8 @@
 const Koa = require('koa')
-
-const config = require('./config')
-
 const app = new Koa()
-
+const bodyParser = require('koa-bodyparser');
+const config = require('./config')
+app.use(bodyParser()) // 解析请求体
 const router = require('./routes')
 
 app.use(router.routes())
--- mpvue-shop-node/routes/index.js
+++ mpvue-shop-node/routes/index.js
@@ -10,4 +10,7 @@ router.get('/index/index',controllers.home.index)
 // 搜索相关的接口
 router.get('/search/indexaction', controllers.search.index.indexaction)
 router.post('/search/addhistoryaction', controllers.search.index.addHistoryAction)
+router.post('/search/clearhistoryAction', controllers.search.index.clearhistoryAction)//清除搜索历史记录
+router.get('/search/helperaction', controllers.search.index.helperAction)
+
 module.exports = router
